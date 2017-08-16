ee0749c990b020f8c714e597e71a9d7b0f981596
pablo berrilio <pabloberrilio@gmail.com>
2017-08-16 11:35:04 -0300

    Fix redisClusterAsyncDisconnect memory leak

--- hircluster.c
+++ hircluster.c
@@ -4944,6 +4944,8 @@ void redisClusterAsyncDisconnect(redisClusterAsyncContext *acc) {
 
         node->acon = NULL;
     }
+
+    dictReleaseIterator(di);
 }
 
 void redisClusterAsyncFree(redisClusterAsyncContext *acc)
